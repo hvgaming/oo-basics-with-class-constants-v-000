a5b85fd9b818e18b32ed403a9f618a2ae5caefa6
hugo valois <hvgaming@yahoo.com>
2018-09-27 22:05:02 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,8 +10,8 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  def brand=(brand)
-    @genre = brand
-    BRAND << brand 
+  def brands=(brands)
+    @genre = brands
+    BRAND << brands 
   end
 end
